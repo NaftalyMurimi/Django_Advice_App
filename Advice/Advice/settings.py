3806f25d3c44278e662c6bcb0e4995447674e70e
NaflayM <nafmurimi@gmail.com>
2024-07-18 10:49:55 +0300

    all vercel

--- Advice/Advice/settings.py
+++ Advice/Advice/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -103,12 +103,18 @@ WSGI_APPLICATION = 'Advice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'aiNQyiLXANbtQUMWnFpTpowRXZYGYfwX',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '14361',
     }
 }
+
 
 
 # Password validation
@@ -146,7 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
