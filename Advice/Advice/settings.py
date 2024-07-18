4e086c4069389966be9c2356ceb3fb42e6e4181d
NaflayM <nafmurimi@gmail.com>
2024-07-18 09:55:06 +0300

    I hope it works

--- Advice/Advice/settings.py
+++ Advice/Advice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z+8h1s=9-v$!b1$gofxjt5dfr3wibvuejv!2p73%t(=yx=hxg^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 ###google api
 SITE_ID = 1
 # Application definition
